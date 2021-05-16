edb6b46d81929ea2f5b47b86a85e22c555969dd0
RayhanHamada <hamadadev2016@gmail.com>
2021-05-16 16:11:31 +0700

    feat: add assets and splashscreen

--- lib/pages/page_splashscreen.dart
+++ lib/pages/page_splashscreen.dart
@@ -1,13 +1,48 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
-class SplashScreenPage extends StatelessWidget {
+class SplashScreenPage extends StatefulWidget {
+  @override
+  _SplashScreenPageState createState() => _SplashScreenPageState();
+}
+
+class _SplashScreenPageState extends State<SplashScreenPage> {
+  void initState() {
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.blue,
       body: Center(
-        child: Column(
-          children: [],
+        child: Padding(
+          padding: const EdgeInsets.only(top: 100.0),
+          child: Column(
+            children: [
+              Container(
+                height: 140.0,
+                width: 140.0,
+                margin: const EdgeInsets.only(
+                  bottom: 50.0,
+                ),
+                child: Image.asset(
+                  'assets/logo.png',
+                  fit: BoxFit.cover,
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Text(
+                  'Bakti Karya',
+                  style: GoogleFonts.roboto(
+                    fontSize: 32,
+                    color: Colors.white,
+                  ),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
