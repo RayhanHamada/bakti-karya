670a11784c531ceb7a9761e29e3d7490221d5d6d
RayhanHamada <hamadadev2016@gmail.com>
2021-06-02 16:26:07 +0700

    feat: make login progress indicator on stacked widget instead on column

--- lib/pages/login_page/login_page.dart
+++ lib/pages/login_page/login_page.dart
@@ -65,49 +65,55 @@ class _LoginPageState extends State<LoginPage> {
     if (_formKey.currentState!.validate()) {
       var email = _emailTextController.text;
       var password = _passwordTextController.text;
-      UserCredential userCred;
 
       try {
         setState(() {
           _isFetchingData = true;
-          _fetchingMsg = 'Please Wait....';
+          _fetchingMsg = 'Logging in, Please Wait....';
         });
 
-        print('$email $password');
-        userCred = await fireAuth.signInWithEmailAndPassword(
+        await fireAuth.signInWithEmailAndPassword(
           email: email,
           password: password,
         );
       } on FirebaseAuthException {
         setState(() {
-          _isFetchingData = false;
+          _isErrorLogin = true;
           _fetchingMsg = 'Invalid email or password, please try again !';
-          _isErrorLogin = !_isErrorLogin;
         });
 
-        _emailTextController.clear();
-        _passwordTextController.clear();
-
-        Future.delayed(
-          Duration(seconds: 3),
-          () {
-            setState(() {
-              _fetchingMsg = '';
-              _isErrorLogin = !_isErrorLogin;
-            });
-          },
-        );
+        _formKey.currentState?.reset();
       }
 
-      setState(() {
-        _isFetchingData = false;
-        _fetchingMsg = 'Success';
-      });
+      if (!_isErrorLogin) {
+        setState(() {
+          _fetchingMsg = 'Success logging in !';
+        });
+      }
+
+      Future.delayed(
+        Duration(seconds: 1),
+        () {
+          setState(() {
+            _isFetchingData = false;
+          });
+        },
+      );
 
       if (fireAuth.currentUser != null) {
-        Navigator.pushNamed(context, '/home');
+        Navigator.pushReplacementNamed(context, '/home');
       }
     }
+  }
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
   }
 
   @override
@@ -116,165 +122,184 @@ class _LoginPageState extends State<LoginPage> {
       /**
         * root
         */
-      body: SingleChildScrollView(
-        child: Center(
-          child: Padding(
-            padding: const EdgeInsets.only(
-              top: 75.0,
+      body: Stack(
+        children: [
+          SingleChildScrollView(
+            child: Center(
+              child: Padding(
+                padding: const EdgeInsets.only(
+                  top: 75.0,
+                ),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  children: [
+                    Container(
+                      height: 100.0,
+                      width: 100.0,
+                      margin: const EdgeInsets.only(
+                        bottom: 50.0,
+                      ),
+                      child: Image.asset(
+                        'assets/logo.png',
+                        fit: BoxFit.contain,
+                      ),
+                    ),
+                    Center(
+                      child: Text(
+                        'Login',
+                        style: TextStyle(
+                          color: Colors.black,
+                          fontSize: 24,
+                        ),
+                      ),
+                    ),
+
+                    /**
+                   * mulai form
+                   */
+                    Form(
+                      autovalidateMode: AutovalidateMode.onUserInteraction,
+                      key: _formKey,
+                      child: Padding(
+                        padding: const EdgeInsets.only(
+                          top: 20.0,
+                          left: 20.0,
+                          right: 20.0,
+                        ),
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          crossAxisAlignment: CrossAxisAlignment.stretch,
+                          children: [
+                            /**
+                           * Field Email
+                           */
+                            TextFormField(
+                              enabled: !_isFetchingData,
+                              controller: _emailTextController,
+                              validator: _validateEmail,
+                              keyboardType: TextInputType.emailAddress,
+                              decoration: InputDecoration(
+                                labelText: 'Email',
+                                border: OutlineInputBorder(
+                                  borderRadius: BorderRadius.circular(
+                                    20,
+                                  ),
+                                ),
+                                prefixIcon: Icon(
+                                  Icons.email,
+                                ),
+                              ),
+                            ),
+                            SizedBox(
+                              height: 20,
+                            ),
+
+                            /**
+                           * Field password
+                           */
+                            TextFormField(
+                              enabled: !_isFetchingData,
+                              controller: _passwordTextController,
+                              validator: _validatePassword,
+                              keyboardType: TextInputType.visiblePassword,
+                              decoration: InputDecoration(
+                                labelText: 'Password',
+                                border: OutlineInputBorder(
+                                  borderRadius: BorderRadius.circular(
+                                    20,
+                                  ),
+                                ),
+                                prefixIcon: Icon(
+                                  Icons.lock_outline,
+                                ),
+                                suffixIcon: IconButton(
+                                  icon: Icon(
+                                    _obscurePassword
+                                        ? Icons.remove_red_eye_outlined
+                                        : Icons.remove_red_eye,
+                                  ),
+                                  onPressed: _toggleObscurePassword,
+                                ),
+                              ),
+                              obscureText: _obscurePassword,
+                            ),
+                            SizedBox(
+                              height: 20,
+                            ),
+
+                            /**
+                           * Tombol Login
+                           */
+                            Container(
+                              width: double.infinity,
+                              child: ElevatedButton(
+                                child: Text(
+                                  'Login',
+                                  style: TextStyle(
+                                    color: Colors.white,
+                                  ),
+                                ),
+                                onPressed: _isFetchingData
+                                    ? null
+                                    : _navigateToHomePage,
+                              ),
+                            ),
+
+                            /**
+                           * Pemisah Login dan Register
+                           */
+                            Padding(
+                              padding: const EdgeInsets.only(
+                                top: 10.0,
+                              ),
+                              child: Row(
+                                children: <Widget>[
+                                  Expanded(
+                                    child: Divider(
+                                      color: Colors.black,
+                                    ),
+                                  ),
+                                  Text("OR"),
+                                  Expanded(
+                                    child: Divider(
+                                      color: Colors.black,
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ),
+
+                            /**
+                           * Tombol Register
+                           */
+                            Container(
+                              width: double.infinity,
+                              child: OutlinedButton(
+                                child: Text(
+                                  'Register',
+                                  style: TextStyle(
+                                    color: Colors.black,
+                                  ),
+                                ),
+                                onPressed: _navigateToRegisterPage,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
             ),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.stretch,
-              children: [
-                Container(
-                  height: 100.0,
-                  width: 100.0,
-                  margin: const EdgeInsets.only(
-                    bottom: 50.0,
-                  ),
-                  child: Image.asset(
-                    'assets/logo.png',
-                    fit: BoxFit.contain,
-                  ),
-                ),
-                Center(
-                  child: Text(
-                    'Login',
-                    style: TextStyle(
-                      color: Colors.black,
-                      fontSize: 24,
-                    ),
-                  ),
-                ),
-
-                /**
-                 * mulai form
-                 */
-                Form(
-                  autovalidateMode: AutovalidateMode.onUserInteraction,
-                  key: _formKey,
-                  child: Padding(
-                    padding: const EdgeInsets.only(
-                      top: 20.0,
-                      left: 20.0,
-                      right: 20.0,
-                    ),
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      crossAxisAlignment: CrossAxisAlignment.stretch,
-                      children: [
-                        /**
-                         * Field Email
-                         */
-                        TextFormField(
-                          controller: _emailTextController,
-                          validator: _validateEmail,
-                          keyboardType: TextInputType.emailAddress,
-                          decoration: InputDecoration(
-                            labelText: 'Email',
-                            border: OutlineInputBorder(
-                              borderRadius: BorderRadius.circular(
-                                20,
-                              ),
-                            ),
-                            prefixIcon: Icon(
-                              Icons.email,
-                            ),
-                          ),
-                        ),
-                        SizedBox(
-                          height: 20,
-                        ),
-
-                        /**
-                         * Field password
-                         */
-                        TextFormField(
-                          controller: _passwordTextController,
-                          validator: _validatePassword,
-                          keyboardType: TextInputType.visiblePassword,
-                          decoration: InputDecoration(
-                            labelText: 'Password',
-                            border: OutlineInputBorder(
-                              borderRadius: BorderRadius.circular(
-                                20,
-                              ),
-                            ),
-                            prefixIcon: Icon(
-                              Icons.lock_outline,
-                            ),
-                            suffixIcon: IconButton(
-                              icon: Icon(
-                                _obscurePassword
-                                    ? Icons.remove_red_eye_outlined
-                                    : Icons.remove_red_eye,
-                              ),
-                              onPressed: _toggleObscurePassword,
-                            ),
-                          ),
-                          obscureText: _obscurePassword,
-                        ),
-                        SizedBox(
-                          height: 20,
-                        ),
-
-                        /**
-                         * Tombol Login
-                         */
-                        Container(
-                          width: double.infinity,
-                          child: ElevatedButton(
-                            child: Text(
-                              'Login',
-                              style: TextStyle(
-                                color: Colors.white,
-                              ),
-                            ),
-                            onPressed: _navigateToHomePage,
-                          ),
-                        ),
-
-                        /**
-                         * Pemisah Login dan Register
-                         */
-                        Padding(
-                          padding: const EdgeInsets.only(
-                            top: 10.0,
-                          ),
-                          child: Row(children: <Widget>[
-                            Expanded(
-                              child: Divider(
-                                color: Colors.black,
-                              ),
-                            ),
-                            Text("OR"),
-                            Expanded(
-                              child: Divider(
-                                color: Colors.black,
-                              ),
-                            ),
-                          ]),
-                        ),
-
-                        /**
-                         * Tombol Register
-                         */
-                        Container(
-                          width: double.infinity,
-                          child: OutlinedButton(
-                            child: Text(
-                              'Register',
-                              style: TextStyle(
-                                color: Colors.black,
-                              ),
-                            ),
-                            onPressed: _navigateToRegisterPage,
-                          ),
-                        ),
-                      ],
-                    ),
-                  ),
-                ),
-                if (_isFetchingData)
+          ),
+          if (_isFetchingData)
+            Container(
+              color: Colors.white.withOpacity(0.8),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
                   Center(
                     child: Container(
                       padding: const EdgeInsets.only(
@@ -284,23 +309,23 @@ class _LoginPageState extends State<LoginPage> {
                       child: CircularProgressIndicator(),
                     ),
                   ),
-                Padding(
-                  padding: const EdgeInsets.only(
-                    top: 20.0,
-                  ),
-                  child: Center(
-                    child: Text(
-                      _fetchingMsg,
-                      style: TextStyle(
-                        color: _isErrorLogin ? Colors.red : Colors.blue,
+                  Padding(
+                    padding: const EdgeInsets.only(
+                      top: 20.0,
+                    ),
+                    child: Center(
+                      child: Text(
+                        _fetchingMsg,
+                        style: TextStyle(
+                          color: Colors.blue,
+                        ),
                       ),
                     ),
                   ),
-                ),
-              ],
+                ],
+              ),
             ),
-          ),
-        ),
+        ],
       ),
     );
   }
