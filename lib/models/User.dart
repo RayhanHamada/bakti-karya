f878c2405ca30449c9b63ec7ab83eebaaa3252a9
RayhanHamada <hamadadev2016@gmail.com>
2021-06-02 14:07:15 +0700

    feat: almost finish register_page register operation

--- lib/models/User.dart
+++ lib/models/User.dart
@@ -1,17 +1,36 @@
 import 'package:bakti_karya/models/CheckoutItem.dart';
 
-class User {
-  User({
+class UserData {
+  UserData({
     required this.email,
-    required this.nama,
+    required this.name,
     required this.noHp,
     required this.alamat,
   });
 
   final String email;
-  String nama;
+  String name;
   String noHp;
   String alamat;
-
   List<CheckoutItem> checkoutItems = [];
+
+  Map<String, dynamic> toJSON() {
+    return {
+      'email': this.email,
+      'name': this.name,
+      'no_hp': this.noHp,
+      'alamat': this.alamat,
+    };
+  }
+
+  /// untuk bikin data awal user di firestore
+  Map<String, dynamic> forCreateToFirestore() {
+    return {
+      'email': this.email,
+      'name': this.name,
+      'no_hp': this.noHp,
+      'alamat': this.alamat,
+      'current_checkout_items': [],
+    };
+  }
 }
--- lib/pages/register_page/register_page.dart
+++ lib/pages/register_page/register_page.dart
@@ -1,3 +1,6 @@
+import 'package:bakti_karya/firebase.dart';
+import 'package:bakti_karya/models/User.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
 class RegisterPage extends StatefulWidget {
@@ -15,6 +18,9 @@ class _RegisterPageState extends State<RegisterPage> {
   var _passwordTextController = TextEditingController();
 
   var _obscurePassword = true;
+  var _isFetchingData = false;
+  var _fetchingMsg = '';
+  var _isErrorRegister = false;
 
   /// atur blur password
   void _toggleObscurePassword() {
@@ -91,6 +97,75 @@ class _RegisterPageState extends State<RegisterPage> {
     Navigator.pop(context);
   }
 
+  Future<void> _registerUser() async {
+    if (_formKey.currentState!.validate()) {
+      var nama = _namaTextController.text;
+      var noHp = _noHpTextController.text;
+      var alamat = _alamatTextController.text;
+      var email = _emailTextController.text;
+      var password = _passwordTextController.text;
+
+      var userData = UserData(
+        email: email,
+        name: nama,
+        noHp: noHp,
+        alamat: alamat,
+      );
+
+      try {
+        setState(() {
+          _isFetchingData = true;
+          _fetchingMsg = 'Registering user, please wait...';
+        });
+
+        await fireAuth.createUserWithEmailAndPassword(
+          email: email,
+          password: password,
+        );
+      } on FirebaseAuthException catch (e) {
+        print(e.code);
+
+        setState(() {
+          _isErrorRegister = true;
+          _fetchingMsg = 'Invalid form data, please try again.';
+        });
+      }
+
+      // jika sukses register user, bikin data user di firestore
+      if (!_isErrorRegister) {
+        await firestore
+            .collection('/users')
+            .add(userData.forCreateToFirestore())
+            .then((value) {
+          setState(() {
+            _fetchingMsg = 'Success Registering User !';
+
+            // clear semua value di form
+            _formKey.currentState!.reset();
+          });
+        });
+      }
+
+      Future.delayed(
+        Duration(
+          seconds: 1,
+        ),
+        () {
+          setState(() {
+            _isErrorRegister = false;
+            _fetchingMsg = '';
+            _isFetchingData = false;
+          });
+
+          // jika tidak error langsung ke login page
+          if (!_isErrorRegister) {
+            _navigateToLogin();
+          }
+        },
+      );
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -113,190 +188,233 @@ class _RegisterPageState extends State<RegisterPage> {
       ),
 
       /// component paling atas
-      body: SingleChildScrollView(
-        child: Center(
-          child: Padding(
-            padding: const EdgeInsets.only(
-              top: 20.0,
-            ),
-            child: Column(
-              children: [
-                Center(
-                  child: Text(
-                    'Register',
-                    style: TextStyle(
-                      color: Colors.black,
-                      fontSize: 24,
-                    ),
-                  ),
+      body: Stack(
+        children: <Widget>[
+          SingleChildScrollView(
+            child: Center(
+              child: Padding(
+                padding: const EdgeInsets.only(
+                  top: 20.0,
                 ),
-
-                /// komponen form
-                Form(
-                  autovalidateMode: AutovalidateMode.onUserInteraction,
-                  key: _formKey,
-                  child: Padding(
-                    padding: const EdgeInsets.only(
-                      top: 20.0,
-                      left: 20.0,
-                      right: 20.0,
+                child: Column(
+                  children: [
+                    Center(
+                      child: Text(
+                        'Register',
+                        style: TextStyle(
+                          color: Colors.black,
+                          fontSize: 24,
+                        ),
+                      ),
                     ),
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      crossAxisAlignment: CrossAxisAlignment.stretch,
-                      children: [
-                        /// input field untuk nama
-                        TextFormField(
-                          validator: _validateNama,
-                          controller: _namaTextController,
-                          decoration: InputDecoration(
-                            labelText: 'Nama',
-                            border: OutlineInputBorder(
-                              borderRadius: BorderRadius.circular(
-                                20,
-                              ),
-                            ),
-                            prefixIcon: Icon(
-                              Icons.person_outline,
-                            ),
-                          ),
-                        ),
-                        SizedBox(
-                          height: 20,
-                        ),
 
-                        /// input field untuk nomer handphone
-                        TextFormField(
-                          keyboardType: TextInputType.phone,
-                          validator: _validateNoHp,
-                          controller: _noHpTextController,
-                          decoration: InputDecoration(
-                            labelText: 'No. Handphone',
-                            border: OutlineInputBorder(
-                              borderRadius: BorderRadius.circular(
-                                20,
+                    /// komponen form
+                    Form(
+                      autovalidateMode: AutovalidateMode.onUserInteraction,
+                      key: _formKey,
+                      child: Padding(
+                        padding: const EdgeInsets.only(
+                          top: 20.0,
+                          left: 20.0,
+                          right: 20.0,
+                        ),
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          crossAxisAlignment: CrossAxisAlignment.stretch,
+                          children: [
+                            /// input field untuk nama
+                            TextFormField(
+                              enabled: !_isFetchingData,
+                              validator: _validateNama,
+                              controller: _namaTextController,
+                              decoration: InputDecoration(
+                                labelText: 'Nama',
+                                border: OutlineInputBorder(
+                                  borderRadius: BorderRadius.circular(
+                                    20,
+                                  ),
+                                ),
+                                prefixIcon: Icon(
+                                  Icons.person_outline,
+                                ),
                               ),
                             ),
-                            prefixIcon: Icon(Icons.phone_android_outlined),
-                          ),
-                        ),
-                        SizedBox(
-                          height: 20,
-                        ),
+                            SizedBox(
+                              height: 20,
+                            ),
 
-                        /// input field untuk alamat rumah
-                        TextFormField(
-                          validator: _validateAlamat,
-                          controller: _alamatTextController,
-                          decoration: InputDecoration(
-                            labelText: 'Alamat Rumah',
-                            border: OutlineInputBorder(
-                              borderRadius: BorderRadius.circular(
-                                20,
+                            /// input field untuk nomer handphone
+                            TextFormField(
+                              enabled: !_isFetchingData,
+                              keyboardType: TextInputType.phone,
+                              validator: _validateNoHp,
+                              controller: _noHpTextController,
+                              decoration: InputDecoration(
+                                labelText: 'No. Handphone',
+                                border: OutlineInputBorder(
+                                  borderRadius: BorderRadius.circular(
+                                    20,
+                                  ),
+                                ),
+                                prefixIcon: Icon(Icons.phone_android_outlined),
                               ),
                             ),
-                            prefixIcon: Icon(Icons.home_outlined),
-                          ),
-                        ),
-                        SizedBox(
-                          height: 20,
-                        ),
+                            SizedBox(
+                              height: 20,
+                            ),
 
-                        /// input field untuk email
-                        TextFormField(
-                          keyboardType: TextInputType.emailAddress,
-                          validator: _validateEmail,
-                          controller: _emailTextController,
-                          decoration: InputDecoration(
-                            labelText: 'Email',
-                            border: OutlineInputBorder(
-                              borderRadius: BorderRadius.circular(
-                                20,
+                            /// input field untuk alamat rumah
+                            TextFormField(
+                              enabled: !_isFetchingData,
+                              validator: _validateAlamat,
+                              controller: _alamatTextController,
+                              decoration: InputDecoration(
+                                labelText: 'Alamat Rumah',
+                                border: OutlineInputBorder(
+                                  borderRadius: BorderRadius.circular(
+                                    20,
+                                  ),
+                                ),
+                                prefixIcon: Icon(Icons.home_outlined),
                               ),
                             ),
-                            prefixIcon: Icon(Icons.email_outlined),
-                          ),
-                        ),
-                        SizedBox(
-                          height: 20,
-                        ),
+                            SizedBox(
+                              height: 20,
+                            ),
 
-                        /// input field untuk password
-                        TextFormField(
-                          controller: _passwordTextController,
-                          validator: _validatePassword,
-                          keyboardType: TextInputType.visiblePassword,
-                          decoration: InputDecoration(
-                            labelText: 'Password',
-                            border: OutlineInputBorder(
-                              borderRadius: BorderRadius.circular(
-                                20,
+                            /// input field untuk email
+                            TextFormField(
+                              enabled: !_isFetchingData,
+                              keyboardType: TextInputType.emailAddress,
+                              validator: _validateEmail,
+                              controller: _emailTextController,
+                              decoration: InputDecoration(
+                                labelText: 'Email',
+                                border: OutlineInputBorder(
+                                  borderRadius: BorderRadius.circular(
+                                    20,
+                                  ),
+                                ),
+                                prefixIcon: Icon(Icons.email_outlined),
                               ),
                             ),
-                            prefixIcon: Icon(Icons.lock_outline),
-                            suffixIcon: IconButton(
-                              icon: Icon(
-                                _obscurePassword
-                                    ? Icons.remove_red_eye_outlined
-                                    : Icons.remove_red_eye,
-                              ),
-                              onPressed: _toggleObscurePassword,
+                            SizedBox(
+                              height: 20,
                             ),
-                          ),
-                          obscureText: _obscurePassword,
-                        ),
-                        SizedBox(
-                          height: 20,
-                        ),
 
-                        /// input field untuk konfirmasi password
-                        TextFormField(
-                          validator: _validatePasswordConfirmation,
-                          keyboardType: TextInputType.visiblePassword,
-                          decoration: InputDecoration(
-                            labelText: 'Password Confirmation',
-                            border: OutlineInputBorder(
-                              borderRadius: BorderRadius.circular(
-                                20,
+                            /// input field untuk password
+                            TextFormField(
+                              enabled: !_isFetchingData,
+                              controller: _passwordTextController,
+                              validator: _validatePassword,
+                              keyboardType: TextInputType.visiblePassword,
+                              decoration: InputDecoration(
+                                labelText: 'Password',
+                                border: OutlineInputBorder(
+                                  borderRadius: BorderRadius.circular(
+                                    20,
+                                  ),
+                                ),
+                                prefixIcon: Icon(Icons.lock_outline),
+                                suffixIcon: IconButton(
+                                  icon: Icon(
+                                    _obscurePassword
+                                        ? Icons.remove_red_eye_outlined
+                                        : Icons.remove_red_eye,
+                                  ),
+                                  onPressed: _toggleObscurePassword,
+                                ),
                               ),
+                              obscureText: _obscurePassword,
                             ),
-                            prefixIcon: Icon(Icons.lock_outline),
-                            suffixIcon: IconButton(
-                              icon: Icon(
-                                _obscurePassword
-                                    ? Icons.remove_red_eye_outlined
-                                    : Icons.remove_red_eye,
-                              ),
-                              onPressed: _toggleObscurePassword,
+                            SizedBox(
+                              height: 20,
                             ),
-                          ),
-                          obscureText: _obscurePassword,
-                        ),
-                        SizedBox(
-                          height: 20,
-                        ),
 
-                        /// Tombol Register
-                        Container(
-                          width: double.infinity,
-                          child: ElevatedButton(
-                            child: Text(
-                              'Register',
-                              style: TextStyle(
-                                color: Colors.white,
+                            /// input field untuk konfirmasi password
+                            TextFormField(
+                              enabled: !_isFetchingData,
+                              validator: _validatePasswordConfirmation,
+                              keyboardType: TextInputType.visiblePassword,
+                              decoration: InputDecoration(
+                                labelText: 'Password Confirmation',
+                                border: OutlineInputBorder(
+                                  borderRadius: BorderRadius.circular(
+                                    20,
+                                  ),
+                                ),
+                                prefixIcon: Icon(Icons.lock_outline),
+                                suffixIcon: IconButton(
+                                  icon: Icon(
+                                    _obscurePassword
+                                        ? Icons.remove_red_eye_outlined
+                                        : Icons.remove_red_eye,
+                                  ),
+                                  onPressed: _toggleObscurePassword,
+                                ),
+                              ),
+                              obscureText: _obscurePassword,
+                            ),
+                            SizedBox(
+                              height: 20,
+                            ),
+
+                            /// Tombol Register
+                            Container(
+                              width: double.infinity,
+                              child: ElevatedButton(
+                                child: Text(
+                                  'Register',
+                                  style: TextStyle(
+                                    color: Colors.white,
+                                  ),
+                                ),
+                                onPressed:
+                                    _isFetchingData ? null : _registerUser,
                               ),
                             ),
-                            onPressed: () {},
-                          ),
+                          ],
                         ),
-                      ],
+                      ),
                     ),
-                  ),
+                  ],
                 ),
-              ],
+              ),
             ),
           ),
-        ),
+          if (_isFetchingData)
+            Container(
+              color: Colors.white.withOpacity(0.8),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Center(
+                    child: Container(
+                      padding: const EdgeInsets.only(
+                        top: 30.0,
+                      ),
+                      alignment: Alignment.center,
+                      child: CircularProgressIndicator(),
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(
+                      top: 20.0,
+                    ),
+                    child: Center(
+                      child: Text(
+                        _fetchingMsg,
+                        style: TextStyle(
+                          color: Colors.blue,
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+        ],
       ),
     );
   }
